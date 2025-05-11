f0e3c55da6ab6a472ef624de68cf001d9ba4e330
MaxwellJryao <maxwell.jryao@gmail.com>
2025-05-11 03:50:51 -0500

    add fsdp

--- distributed/run.sh
+++ distributed/run.sh
@@ -2,7 +2,10 @@
 export MASTER_ADDR="127.0.0.1"
 export MASTER_PORT="12355"
 
-python distributed_data_parallel.py
+# python distributed_data_parallel.py
 
 # torchrun ddp
-torchrun --standalone --nproc_per_node=2 ddp_torchrun.py 10 10
+# torchrun --standalone --nproc_per_node=2 ddp_torchrun.py 10 10
+
+# fsdp
+python fsdp.py
