7f753828ca9d0f38cc952328b58529819db60a56
MaxwellJryao <maxwell.jryao@gmail.com>
2025-05-11 04:31:55 -0500

    add c++ notes

---
+++ cpp/hello_world/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    int a = 1, b = 2;
+    int c = a + b;
+    cout << "c = " << c << endl;
+    return 0;
+}
